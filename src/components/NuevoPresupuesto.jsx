3471c393161f8d12cc90315bab5812bc5f456424
Gionnelly <80008338+gvielza@users.noreply.github.com>
2022-04-22 14:13:12 -0300

    proyecto Control de Gastos de Udemy

---
+++ src/components/NuevoPresupuesto.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NuevoPresupuesto = () => {
+    return (
+        <div className='contenedor-presupuesto contenedor sombra'>
+            <form className='formulario' action=""></form>
+        </div>
+    );
+};
+
+export default NuevoPresupuesto;
